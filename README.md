286b81b1e58a73e60b72e232d594ee91ffe98504
Pip Jones <scipilot@users.noreply.github.com>
2016-06-23 23:52:08 +1000

    + initial IMPORT - NOT TESTED!

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
 # logline
-A vis.js Timeline wrapper to display log data from various formats and customised providers.
+Log Event Timeline Visualisation - jQuery plugin.
+
+Logline is a Vis.js Timeline wrapper to mash up and display log data from various formats and customised providers.
+
+- Uses the vis.js Timeline library, which is incredibly powerful and intuitive.
+- Mashes up from multiple REST APIs or local data, from injected data-providers.
+- Handles different types of time-ranged events and log formats.
+- Groups logs into vis Timeline groups, with a "Misc" group to catch poorly formatted logs (it happens).
+- Installs popovers for more detail (currently BS2!)
+- Auto-fits the range on data load.
+- Can present loading spinners while API data is loading.
+
+The only dependency vis.js must be loaded first. Oh and JQuery of course.
+
+Logs time formats supported:
+
+     1. "statefully-ranged" : flip-flop/FSM over two logs with one time field and a boolean state field for on/off 
+     2. "pre-ranged" : one log with two time fields, start-end
+     3. "instant" events: not ranged, one log one time field
+
+These formats came from real-world scenarios with various devices producing various log formats. 
+
+The state-ranged logs are where you have two logs per event, one at the start with a state=1 and a date, 
+then another log at the end of event's time period with a state=0. This plugin tracks the state of these events 
+and formats the timeline time-span accordinly. It can deal with intermingled and overlapping events, (as long as they are different).    
+It can also deal with "endless" and "beginningless" events if the time-window chops off one of the pair, or the original data is lossy.
+e.g. If there is no-end and then a new start (in one sub-type) it will auto-close and restart the event, assuming an end 
+(otherwise you'd get an overlapping block to infinity!).
+
+The other two formats are more obvious and simpler to process. Something happened: either at X time or between Y and Z - in one log. 
+
+## Configuration Options
+
+init options (with defaults):
+
+    API.URL: string                   Base URL of the REST API
+    unitId: integer                   ID of the logged device
+    since: Datetime                   Initial time window filter
+    until: Datetime
+    CSS.loading: ('loading')          Class which is applied/removed to the spinner during api loading.
+    CSS.loader: ('loader')            Class of an element into which the spinner will be appended.
+    dataProviders: Array [func, ...]  Callback functions which will return injected data (below)
+
+You can define a 'loading' CSS class e.g. with a spinner, this is added/removed automatically:
+
+    .timeline.loading {
+        background:url(/images/loading.gif) center center no-repeat;
+        background-size: 30px;
+    }
+
+You must inject dataProvider functions which are called in turn to fetch the data, and metadata describing how to map the logs into the timeline.
+
+The dataProvider functions should have this signature:
+
+    provider(unitId, since, until, callback);
+
+The dataProvider functions should asynchronously return this to the callback:
+    
+    callback( ["success"|"error"], {data: [data,...], meta: { <metadata> } )
+
+The data rows can contain any fields (columns), but only those described in the metadata will be interpreted.
+All other fields will be displayed in the pre-formatted "details" popovers for user reference.
+
+The metadata describing the data has the following properties:
+
+     {
+		family: 'X', 					// Unique log family ID
+		apiNoun:'tripLog',				// For API-loaded data, the REST noun to query
+		stateField:'<fieldname>',		// Optional; Which log field flip-flops the start-end state (for state-ranged logs)
+		dateField:'<fieldname>',		// Which log field contains the FSM date; or the start date if endDateField is included
+		endDateField:'<fieldname>',		// Optional; indicates this is a pre-ranged timespan event.
+		logGroupField:'<fieldname>',	// Optional; Which log field is the logGroup [e.g. type|event] for logs with sub-types.
+		logGroups: ['', 'X', 'Y' ...], 	// A list of logGroup (sub-types) found in the logGroupField. Just [''] for logs with no sub-types.
+		groupTitles:{'X':'Foo', ...},	// A map of human titles, indexed by logGroups. Note: instant-events use the event data itself as the title and don't use this!
+		rangedLogs:['Y', ...],  		// Optional; Which of the logGroup sub-types are state-ranged flip-flops. Those not present in this list are treated as instant events.
+		cssClassMap:{'X': 'class', ...} // Optional; map of CSS classes for each logGroup sub-type, applied to the Vis element itself.
+	}
+
+## Usage
+
+e.g.
+
+    <script src="js/vis/vis.min.js"></script>
+    <script src="js/jquery/jquery.pluginmaker.js"></script>
+    <script src="js/jquery.scipilot-logline.js"></script>
+    <link href="js/vis/vis.min.css" rel="stylesheet" type="text/css" />
+
+    $('#timeline').scipilot_logline({
+			API: {URL: 'https://tmapi.mysite.com/'},
+			unitId: 2600,
+			since:  '2016-05-05T12:00:00+10:00',
+			CSS: {loader: 'timeline-loader', loading: 'loading span2 alert alert-info clearfix'}
+		})
+    .scipilot_logline('load');
+
---
+++ src/jquery.scipilot-logline-timeline.js
@@ -0,0 +1,329 @@
+/**
+ * @author Pip Jones
+ * @since  10/06/2016
+ * 
+ * Requires pluginMaker (my version adapted from Jupiter)
+ * 
+ * @todo Assumes bootstrap2! (for popover) needs rendering callback?
+ */
+(function ($) {
+	// Define plugin class.
+	var ScipilotLoglineVisTimeline = function (el, options) {
+		this.settings = {};
+		if (el) this.init(el, options);
+	};
+
+	$.extend(ScipilotLoglineVisTimeline.prototype, {
+		// plugin name
+		name: 'scipilot_logine',
+
+		// the vis timeline object
+		timeline: null,
+		dsItems: null,
+		dsGroups: null,
+
+		aFamilies: [],
+
+		// Define how the events are grouped. Group 0= default/misc if no match on event name.
+		aLogGroups: {},
+		aLogGroupIndex: [], // index built dynamically
+
+		// Content hashes (group titles) indexed by the log group.
+		aGroupTitles: {},
+		// CSS Classes for each log/event type
+		aCssClassMap: {},
+
+		// stores vis.js Timeline options
+		timelineOptions: {},
+
+		// Default settings -----------------------------------------
+		defaults: {
+			API: {
+				URL: ''
+			},
+			unitId: null,
+			since: '',
+			until: '',
+			'CSS': {
+				'loading': 'loading',
+				'loader': 'loader'
+			},
+			dataProviders: []
+		},
+
+		// Plugin instance initialisation and configuration
+		init: function (el, options) {
+			// If options exist, merge them with a copy of the defaults into our instance settings
+			if (options) {
+				this.settings = $.extend({}, this.defaults);
+				$.extend(this.settings, options);
+			}
+
+			// Timeline configuration
+			this.timelineOptions = {
+				// todo: trouble with the height is if the number of groups gets higher, it crops it with no inner scrollbar...
+				//height: window.innerHeight - 200, // todo: calculate, remove header etc. make "full screen" (tried 100%)
+				maxHeight: window.innerHeight, 	// I fixed this which make the UX feel more consistent.
+				stack: false, 					// the timeline seems to make more sense with no stacking, else it gets jumbled.
+				zoomMin: 1000,					// we only have 1s resolution
+				zoomMax: 100000000000, 			// feels reasonable: 3yrs.
+			};
+		},
+
+		// @private call this before processing after adding all families. Can be re-called.
+		// @see getVisGroup() to recalculate these Group Index on the fly.
+		indexFamilyToGroup: function () {
+			// Indexing
+			// todo: this is a bit wasteful, just discards and rebuilds: make it more like "add to index"?
+			this.dsGroups.clear();
+			// Add groups together indexed via family+log group id (to differentiate e.g. 'S').
+			var self = this;
+			$.each(this.aFamilies, function (i, f) {
+				$.each(self.aLogGroups[f], function (j, g) {
+					var id = f + '.' + g;
+					self.dsGroups.add({id: id, content: self.aGroupTitles[f][g], order: i++});
+					self.aLogGroupIndex[id] = self.dsGroups.length;
+				});
+			});
+		},
+
+		// @private call this before processing after adding all families. Can be re-called.
+		// @see getVisGroup() to recalculate these Group Index on the fly.
+		indexGroup: function (meta) {
+			// Add groups together indexed via family+log group id (to differentiate e.g. 'S').
+			$.each(meta.logGroups, $.proxy(function (j, g) {
+				var id = meta.family + '.' + g;
+				// todo add UI group ordering to metadata? currently it's load sequence order
+				this.dsGroups.add({id: id, content: meta.groupTitles[g], order: this.aLogGroupIndex.length});
+				this.aLogGroupIndex[id] = this.dsGroups.length;
+			}, this));
+		},
+
+		// trace helper/wrapper
+		log: function (message) {
+			if (typeof(console) !== 'undefined') {//IE
+				console.log(message);
+			}
+			else {
+				this.log += message + "\n";//IE
+			}
+		},
+
+		/** Updates the API.URL setting */
+		setAPIURL: function (sURL) {
+			this.settings.API.URL = sURL;
+		},
+
+		/** Loads the data from the configured API and displays it in a vis.js Timeline */
+		load: function () {
+			// Create a DataSet (with two-way data binding)
+			this.dsItems = new vis.DataSet();
+			this.dsGroups = new vis.DataSet();
+
+			// todo: use timeline strategy pattern, to enable switch to CHAPS-LINK for clustering, or other library?
+			this.timeline = new vis.Timeline(this.element.get(0), this.dsItems, this.dsGroups, this.timelineOptions);
+
+			// The mashup list...
+			$.each(this.settings.dataProviders, $.proxy(function (i, dp) {
+				this.loadInjectedData(dp);
+			}, this));
+		},
+
+		/**
+		 * @todo define injection pattern (ie. wrap this class in another with the TM injections?)
+		 *
+		 * @param meta Object {family: 'X', logGroupField:'', stateField:'', dateField:'', logGroups: ['',...], groupTitles:{'':'', ...},
+		 * 	rangedLogs:{'':'', ...},  cssClassMap:{'':'', ...} }
+		 */
+		injectMetaData: function (meta) {
+			// todo 'family' could be locally generated on-injection, it's just an index.
+			this.aFamilies.push(meta.family);
+			this.aLogGroups[meta.family] = meta.logGroups;
+			this.aGroupTitles[meta.family] = meta.groupTitles;
+			this.aCssClassMap[meta.family] = meta.cssClassMap;
+
+			// meta reindex this.aLogGroupIndex
+			this.indexFamilyToGroup();
+		},
+
+		/**
+		 * Handles sync/async loading via dataProvider which performs either local or remote data-loading. 
+		 * @param fnDataProvider should return to the callback a data Object {meta:{logGroupField:, stateField:, dateField:, groupTitles:{}, rangedLogs:{}, cssClassMap:{}}, data:{[ row, ... ]}}
+		 */
+		loadInjectedData: function (fnDataProvider) {
+
+			var jLoadingSpinner = $('<div class="' + this.settings.CSS.loading + '">Loading ' + noun + '...</div>')
+				.prependTo($('.' + this.settings.CSS.loader));
+
+			var data = fnDataProvider(
+				this.settings.since,
+				this.settings.until,
+				this.settings.unitId,
+				$.proxy(function (status, data) {
+					if (status == 'success') {
+						this.injectMetaData(data.meta);
+						this.processLogs(data.data, meta);
+					}
+					else {
+						alert('Sorry, the ' + noun + ' data failed to load. (' + status + ')');
+					}
+
+					jLoadingSpinner.remove();
+
+				}, this));
+		},
+
+		/**
+		 * Parses the API response data, scanning for instant-events and ranged-events, adding them to the timeline.
+		 * Handles mixed types and tracks the states of the FSM-ranged-events (single data field, two rows for start-end)
+		 * @param data Array rows of logs
+		 * @param meta Object meta-data structure describing the data (see above)
+		 */
+		processLogs: function (data, meta) {
+			var i, datum, eventStates = [], format, visGroup;
+			var aTitles = meta.groupTitles;
+
+
+			// Parse response, convert to vis.DataSet
+			for (i = 0; i < data.length; i++) {
+				datum = data[i];
+				visGroup = this.getVisGroup(meta.family, datum[meta.logGroupField]);
+
+				// todo move to a processor helper/strategy?
+				// Check for state-ranged time-span events, (Ignition states, Sessions)
+				if (meta.rangedLogs && meta.rangedLogs.indexOf(datum[meta.logGroupField]) != -1) {
+					// 1 starts it, 0 ends it. Note it may "start open" or "never close", in the time window.
+					// Truth table: - = no state recorded, 0/1 = state previously recorded
+					// State	Receive 	Scenario 					Action
+					// -----+----------+-----------------+-------
+					//	-			0					End "start-open"	End new event starting from beginning of window, add to timeline.
+					//									Log error					End new event starting after last similar state event, add to timeline.
+					//	- 		1					Begin state				Begin new event starting now
+					// 	0			0					Error							Delete impossible 0 state
+					// 	0			1					Error							Delete impossible 0 state. Begin new event starting now.
+					//	1			0					End state					End existing event, add to timeline.
+					//	1			1					Log Error?				End existing event, add to timeline. Begin new event starting now.
+					//	0			- 				Wont happen
+					//	1			- 				"never closed"		End existing event, add to timeline.
+					//
+					// The error handling here is in interesting.
+					// - Consecutive 1s or 0s are "wrong" but possible, due to cham_log errors, loss, or same-second events.
+					// - State=0 won't happen as we don't store them here.
+					// - Time-window "start open" scenarios can be indistinguishable from log-errors.
+					// - We need to scan the state cache at the end of the dataset to detect "never close" and close them off.
+
+					// todo initially I'm just doing the happy path! convert to an FSM?
+					if (datum[meta.stateField] == 1) {
+						// Cache the start
+						eventStates[datum[meta.logGroupField]] = datum;
+					}
+					else {
+						// Assume 0 = off
+						if (eventStates[datum[meta.logGroupField]]) {
+							format = this.formatVisContent(meta, datum, aTitles[datum[meta.logGroupField]]);
+							// End it and add ranged event to timeline
+							this.dsItems.add({
+								id: datum.logId,        													// which Log.ID to use: start or end?
+								group: visGroup,
+								start: eventStates[datum[meta.logGroupField]][meta.dateField],
+								end: datum[meta.dateField],
+								content: format.content,
+								className: format.className
+								 //type: 'background' // looks OK too
+								// type: 'box' (default), 'point', 'range', or 'background'
+								// style:
+							});
+							eventStates[datum[meta.logGroupField]] = null; //unset? pop? splice?
+						}
+						//else error / "start-open"
+					}
+				}
+				else {
+					// Single-log event records
+					format = this.formatVisContent(meta, datum, datum[meta.titleField]);
+
+					if(meta.endDateField){
+						// Date-ranged single-log with start-end date
+						this.dsItems.add({
+							id: datum.logId,
+							group: visGroup,
+							start: datum[meta.dateField],
+							end: datum[meta.endDateField],
+							content: format.content,
+							className: format.className
+						});
+					}
+					else {
+						// Instant event, single date, (GPS, Periodic, Alarms)
+						this.dsItems.add({
+							id: datum.logId,
+							group: visGroup,
+							start: datum[meta.dateField],
+							content: format.content,
+							className: format.className
+						});
+					}
+				}
+			}
+			// Re-render the timeline, so the popovers will bind.
+			this.timeline.fit(false);
+			// Bind the log/event tooltip/popovers. container:body solves overflow clipping.
+			// @todo assumes Bootstrap2! inject...
+			$('.timeline-popover').popover({html: true, trigger: "hover", placement: "bottom", container: "body"});
+		},
+
+		/**
+		 * @todo inject a template? or this should be a callback to the page... this JQ plugin shouldn't know about bootstrap.
+		 * @private
+		 * @param meta Object
+		 * @param datum Object log row
+		 * @param title String TODO HTML? SAFE?
+		 * @return Object {content: '<content>', className: '<cssClass>'};
+		 */
+		formatVisContent: function (meta, datum, title) {
+			format = this.formatSpecificLogs(meta, datum);
+			// Uses Bootstrap popover.
+			var content = ''
+				+ '<a class="timeline-popover" '
+				+ ' 	data-content="' + format.content + '" >'
+				+ title
+				+ '<span class="icon icon-info-sign">&nbsp;&nbsp;</span></a>';
+			return {content: content, className: format.className};
+		},
+
+		/**
+		 * This is a log type-specific formatter.
+		 *
+		 * @todo the idea here was to wrap all log types, but I'm now moving towards injecting these dependencies! this may evaporate
+		 *
+		 * @param meta Object
+		 * @param datum Object
+		 */
+		formatSpecificLogs: function (meta, datum) {
+			var logGroup = meta.logGroupField ? datum[meta.logGroupField] : ''; // '' is default for no grouping
+			var content = '<pre>' + JSON.stringify(datum, null, " ").replace(/"/g, '&quot;') + '</pre>';  //pauper's formatter
+			return {
+				content: content,
+				className: meta.cssClassMap[logGroup]
+			};
+		},
+
+		/**
+		 * @param family
+		 * @param event
+		 * @returns {string} Group Index for Vis.js timeline
+		 */
+		getVisGroup: function (family, event) {
+			var id = family + '.' + event;
+
+			// Check group exists, some logs are wrong (e.g. 'E.C' which has the RFID in the event, until it's reworked :-/)
+			// if not found return the family "blank" group (misc)
+			return this.aLogGroupIndex.hasOwnProperty(id) ? id : family + '.';
+		}
+	});
+
+	// --------------------------------------------------------------
+	// Create the JQ plugin from the Class
+	$.pluginMaker(ScipilotLoglineVisTimeline);
+
+})(jQuery);
